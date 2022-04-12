851c3873499ef746d87441a2d9d9b8457aa08897
JL <joona.lepola@gmail.com>
2022-04-12 22:37:56 +0300

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const createReducer = require('./createReducer');
 const createDispatchers = require('./createDispatchers');
 
+/**
+ * Creates Redux store with dispatchers with api calls included
+ * @param {String} name Name of the reducer
+ * @param {String} path API path
+ * @param {Array} wantedEndpoints Array of wanted endpoints
+ * @param {Function} axiosInstance Axios instance
+ * @returns {Object} Reducer and Dispatchers
+ */
 const SRRWAC = (name, path, wantedEndpoints, axiosInstance) => {
   const Reducer = createReducer(name);
   Object.defineProperty(Reducer, 'name', { value: name });
